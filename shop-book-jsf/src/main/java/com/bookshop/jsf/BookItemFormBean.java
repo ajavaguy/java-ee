813e00434483c0573457e50283d558634afa7db3
A Java Guy <ajavaguy96@gmail.com>
2024-04-26 21:34:04 +0700

    Inject

--- shop-book-jsf/src/main/java/com/bookshop/jsf/BookItemFormBean.java
+++ shop-book-jsf/src/main/java/com/bookshop/jsf/BookItemFormBean.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 import com.shopbook.BookItem;
@@ -21,6 +22,9 @@ public class BookItemFormBean implements Serializable {
 	@EJB
 	private ShopBookLocal bookLocal;
 	
+	@Inject
+	private InventoryService inventoryService;
+	
 //	public String addItem() {
 //		long itemId = this.items.size() + 1;
 //		this.items.add(new BookItem(itemId, item.getName(), item.getManufacturer(), item.getDescription(), item.getAvailableDate()));
@@ -30,6 +34,8 @@ public class BookItemFormBean implements Serializable {
 	public String addItem() {
 		long itemId = this.bookLocal.getItems().size() + 1;
 		this.bookLocal.getItems().add(new BookItem(itemId, item.getName(), item.getManufacturer(), item.getDescription(), item.getAvailableDate()));
+		
+		this.inventoryService.createItem(item.getItemId(), item.getName());
 		return "list?faces-redirect=true";
 	}
 	
